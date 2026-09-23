01d00c92c20236328e2cfa77d379e8f4d9872d31
Sutreja-Rutvik <rutvikvsutreja2312@gmail.com>
2026-09-23 22:42:34 +0530

    Lab-13 Uploded

---
+++ Lab-13/L13_B_c_.c
@@ -0,0 +1,24 @@
+/*
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+*/
+
+#include<stdio.h>
+void main(){
+    for (int i=1;i<=5;i++)
+    {
+        for (int k=1;k<=i;k++)
+        {
+            printf(" ");
+        }
+        printf("");
+        for (int j=1;j<=6-i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
